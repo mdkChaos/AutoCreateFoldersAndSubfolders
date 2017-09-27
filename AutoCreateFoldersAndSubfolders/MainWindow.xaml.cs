0b3d306421b275d3d6f792b5f772770ece1f315f
mdkChaos <mdkchaos1@gmail.com>
2017-09-27 22:39:19 +0200

    Wrote programm

--- AutoCreateFoldersAndSubfolders/MainWindow.xaml.cs
+++ AutoCreateFoldersAndSubfolders/MainWindow.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace AutoCreateFoldersAndSubfolders
 {
@@ -23,6 +13,57 @@ namespace AutoCreateFoldersAndSubfolders
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void SelectPath_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new FolderBrowserDialog();
+            DialogResult result = dialog.ShowDialog();
+            if (result == System.Windows.Forms.DialogResult.OK)
+            {
+                PathFolder.Content = dialog.SelectedPath;
+            }
+            else
+            {
+                PathFolder.Content = "The folder was not selected!";
+            }
+        }
+
+        private void CreateFolder_Click(object sender, RoutedEventArgs e)
+        {
+            string path = (string)PathFolder.Content;
+            string folderName = NewFolderName.Text;
+            string subFolderName = NewSubFolderName.Text;
+            int count = Convert.ToInt32(NumberOfSubFolders.Text);
+            if (path != null && folderName != null && subFolderName != null)
+            {
+                string newPath = $"{path}\\{folderName}";
+                if (Directory.Exists(newPath))
+                {
+                    System.Windows.MessageBox.Show("Folder already exists!");
+                    return;
+                }
+                else
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                for (int i = 1; i <= count; i++)
+                {
+                    if (i < 10)
+                    {
+                        Directory.CreateDirectory($"{newPath}\\{subFolderName} 0{i}");
+                    }
+                    else
+                    {
+                        Directory.CreateDirectory($"{newPath}\\{subFolderName} {i}");
+                    }
+                }
+                System.Windows.MessageBox.Show("Create folders completed!");
+                PathFolder.Content = "";
+                NewFolderName.Clear();
+                NewSubFolderName.Clear();
+                NumberOfSubFolders.Clear();
+            }
         }
     }
 }
